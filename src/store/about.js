ebed0dabadd8845239e68d0ff305a5c05c2878d7
peterkkhan <juna08@naver.com>
2025-02-07 14:38:31 +0900

    Create Project

---
+++ src/store/about.js
@@ -0,0 +1,11 @@
+import { Store } from '../core/heropy'
+
+export default new Store({
+  photo: 'https://heropy.blog/css/images/logo.png',
+  name: 'Peterkkhan / Jeonha Kang',
+  email: 'juna08@naver.com',
+  blog: 'https://x.com/peterkkhan',
+  github: 'https://github.com/peterkkhan/',
+  repository: 'https://github.com/peterkkhan/movie-app'
+
+})
