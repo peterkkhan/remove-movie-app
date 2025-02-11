89dd7b7c19f0ab68ca60f441e5f87041b69ae342
peterkkhan <juna08@naver.com>
2025-02-11 11:48:41 +0900

    Complied Typescript

--- src/components/MovieList.ts
+++ src/components/MovieList.ts
@@ -32,9 +32,9 @@ export default class MovieList extends Component {
     )
 
 
-    const loaderEl = this.el.querySelector('.the-loader')
+    const loaderEl = this.el.querySelector('.the-loader') 
     movieStore.state.loading 
-      ? loaderEl.classList.remove('hide')
-      : loaderEl.classList.add('hide')
+      ? loaderEl?.classList.remove('hide')
+      : loaderEl?.classList.add('hide')
   }
 }
